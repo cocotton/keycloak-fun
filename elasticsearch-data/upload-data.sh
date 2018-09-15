77d9fb7936f6378ce04a98a41cfcc6eaca09d693
cocotton <julien.parent.trudeau@gmail.com>
2018-09-15 15:33:40 -0400

    Update upload-data.sh verbosity

--- elasticsearch-data/upload-data.sh
+++ elasticsearch-data/upload-data.sh
@@ -3,13 +3,26 @@
 elasticuser="$1"
 elasticpass="$2"
 
-echo "--- Uncompressing data files"
+printf "Uncompressing data files..."
 unzip -o /root/accounts.zip -d /root
+printf "DONE\n"
 
-echo "--- Waiting for elastic to come online"
-until telnet localhost:9200; do echo "."; sleep 2; done
+printf "Waiting for Elastic to come online..."
+until curl http://elasticsearch:9200 > /dev/null 2>&1; do sleep 2; done
+printf "DONE\n"
 
-echo "--- Uploading test data"
-curl --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPOST -H 'Content-Type: application/x-ndjson' 'localhost:9200/bank/account/_bulk?pretty' --data-binary @accounts.json
+printf "Waiting for Kibana to come online..."
+until curl http://kibana:5601 > /dev/null 2>&1; do sleep 2; done
+printf "DONE\n"
 
-echo "--- Done"
+printf "Uploading test data..."
+curl --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPOST -H 'Content-Type: application/x-ndjson' 'http://elasticsearch:9200/bank/account/_bulk?pretty' --data-binary @/root/accounts.json > /dev/null 2>&1
+printf "DONE\n"
+
+printf "Creating index pattern..."
+curl --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -s -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: index_pattern" \
+    "http://kibana:5601/api/saved_objects/index-pattern/bank\*" \
+    -d "{\"attributes\":{\"title\":\"bank\*\",\"timeFieldName\":\"time\"}}" > /dev/null 2>&1
+printf "DONE\n"
+
+printf "Script completed. Exiting.\n"
