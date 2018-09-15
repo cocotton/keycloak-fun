67f4ecbb3fbf118d4c3a5707ed5cfc221f2c2658
cocotton <julien.parent.trudeau@gmail.com>
2018-09-15 15:04:48 -0400

    Wait for port 9200 to come online

--- elasticsearch-data/upload-data.sh
+++ elasticsearch-data/upload-data.sh
@@ -6,6 +6,9 @@ elasticpass="$2"
 echo "--- Uncompressing data files"
 unzip -o /root/accounts.zip -d /root
 
+echo "--- Waiting for elastic to come online"
+until telnet localhost:9200; do echo "."; sleep 2; done
+
 echo "--- Uploading test data"
 curl --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPOST -H 'Content-Type: application/x-ndjson' 'localhost:9200/bank/account/_bulk?pretty' --data-binary @accounts.json
 
