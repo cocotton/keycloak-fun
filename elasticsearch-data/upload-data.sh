d0069dfdefcb8407762248c042b63514e7002e82
cocotton <julien.parent.trudeau@gmail.com>
2018-09-15 15:50:10 -0400

    Update verbosity and fix index pattern creation

--- elasticsearch-data/upload-data.sh
+++ elasticsearch-data/upload-data.sh
@@ -3,26 +3,26 @@
 elasticuser="$1"
 elasticpass="$2"
 
-printf "Uncompressing data files..."
+echo "Uncompressing data files..."
 unzip -o /root/accounts.zip -d /root
-printf "DONE\n"
+echo "Done"
 
-printf "Waiting for Elastic to come online..."
-until curl http://elasticsearch:9200 > /dev/null 2>&1; do sleep 2; done
-printf "DONE\n"
+echo "Waiting for Elastic to come online..."
+until curl http://elasticsearch:9200 > /dev/null 2>&1; do echo "."; sleep 2; done
+echo "Done"
 
-printf "Waiting for Kibana to come online..."
-until curl http://kibana:5601 > /dev/null 2>&1; do sleep 2; done
-printf "DONE\n"
+echo "Waiting for Kibana to come online..."
+until curl http://kibana:5601 > /dev/null 2>&1; do echo "."; sleep 2; done
+echo "Done"
 
-printf "Uploading test data..."
-curl --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPOST -H 'Content-Type: application/x-ndjson' 'http://elasticsearch:9200/bank/account/_bulk?pretty' --data-binary @/root/accounts.json > /dev/null 2>&1
-printf "DONE\n"
+echo "Uploading test data..."
+curl --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPOST -H 'Content-Type: application/x-ndjson' 'http://elasticsearch:9200/bank/account/_bulk?pretty' --data-binary @/root/accounts.json
+echo "Done"
 
-printf "Creating index pattern..."
+echo "Creating index pattern..."
 curl --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -s -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: index_pattern" \
-    "http://kibana:5601/api/saved_objects/index-pattern/bank\*" \
-    -d "{\"attributes\":{\"title\":\"bank\*\",\"timeFieldName\":\"time\"}}" > /dev/null 2>&1
-printf "DONE\n"
+    "http://kibana:5601/api/saved_objects/index-pattern/bank*" \
+    -d "{\"attributes\":{\"title\":\"bank*\"}}"
+echo "Done"
 
-printf "Script completed. Exiting.\n"
+echo "Script completed. Exiting."
