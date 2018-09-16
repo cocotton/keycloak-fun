b5745b7b465c8b3ca22bf654a37770e1333e8cd8
cocotton <julien.parent.trudeau@gmail.com>
2018-09-15 20:32:45 -0400

    Use https:// in upload-data.sh script

--- elasticsearch-data/upload-data.sh
+++ elasticsearch-data/upload-data.sh
@@ -8,19 +8,19 @@ unzip -o /root/accounts.zip -d /root
 echo "Done"
 
 echo "Waiting for Elastic to come online..."
-until curl http://elasticsearch:9200 > /dev/null 2>&1; do echo "."; sleep 2; done
+until curl https://elasticsearch:9200 > /dev/null 2>&1; do echo "."; sleep 2; done
 echo "Done"
 
 echo "Waiting for Kibana to come online..."
-until curl http://kibana:5601 > /dev/null 2>&1; do echo "."; sleep 2; done
+until curl https://kibana:5601 > /dev/null 2>&1; do echo "."; sleep 2; done
 echo "Done"
 
 echo "Uploading test data..."
-curl --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPOST -H 'Content-Type: application/x-ndjson' 'http://elasticsearch:9200/bank/account/_bulk?pretty' --data-binary @/root/accounts.json
+curl -k --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPOST -H 'Content-Type: application/x-ndjson' 'http://elasticsearch:9200/bank/account/_bulk?pretty' --data-binary @/root/accounts.json
 echo "Done"
 
 echo "Creating index pattern..."
-curl --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -s -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: index_pattern" \
+curl -k --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -s -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: index_pattern" \
     "http://kibana:5601/api/saved_objects/index-pattern/bank*" \
     -d "{\"attributes\":{\"title\":\"bank*\"}}"
 echo "Done"
