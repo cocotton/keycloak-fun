0d0ceb4db426596ccd6e7957b77be9f77a4b3093
cocotton <julien.parent.trudeau@gmail.com>
2018-09-15 20:47:58 -0400

    Fix elastic TLS config

--- elasticsearch-data/upload-data.sh
+++ elasticsearch-data/upload-data.sh
@@ -8,11 +8,11 @@ unzip -o /root/accounts.zip -d /root
 echo "Done"
 
 echo "Waiting for Elastic to come online..."
-until curl https://elasticsearch:9200 > /dev/null 2>&1; do echo "."; sleep 2; done
+until curl -k https://elasticsearch:9200 > /dev/null 2>&1; do echo "."; sleep 2; done
 echo "Done"
 
 echo "Waiting for Kibana to come online..."
-until curl https://kibana:5601 > /dev/null 2>&1; do echo "."; sleep 2; done
+until curl -k https://kibana:5601 > /dev/null 2>&1; do echo "."; sleep 2; done
 echo "Done"
 
 echo "Uploading test data..."
