15417227f702a7258b6b60aac25453a9633c6b97
cocotton <julien.parent.trudeau@gmail.com>
2018-09-17 21:25:55 -0400

    Silence curl call

--- keycloak-setup/keycloak_setup.sh
+++ keycloak-setup/keycloak_setup.sh
@@ -7,7 +7,7 @@ echo "Getting bearer token..."
 bearer_token=`curl -ks "https://keycloak:8443/auth/realms/master/protocol/openid-connect/token" -d "client_id=admin-cli" -d "username=${KEYCLOAK_USERNAME}" -d "password=${KEYCLOAK_PASSWORD}" -d "grant_type=password" | jq -r '.access_token'`
 
 echo "Uploading Demo realm..."
-curl -k -XPOST -H "Authorization: bearer ${bearer_token}" -H "Content-Type: application/json" "https://keycloak:8443/auth/admin/realms" \
+curl -ks -XPOST -H "Authorization: bearer ${bearer_token}" -H "Content-Type: application/json" "https://keycloak:8443/auth/admin/realms" \
  -d @/root/realm-export.json
 
 echo "Creating demo user..."
