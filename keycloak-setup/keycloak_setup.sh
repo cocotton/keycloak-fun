290111f22a4c1332dd95f57f71735930d57e4e3e
cocotton <julien.parent.trudeau@gmail.com>
2018-09-17 20:36:13 -0400

    Add verbosity to keycloak_setup.sh

--- keycloak-setup/keycloak_setup.sh
+++ keycloak-setup/keycloak_setup.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
-bearer_token=`curl -ks "https://keycloak:8443/auth/realms/master/protocol/openid-connect/token" -d "client_id=admin-cli" -d "username=${KEYCLOAK_USERNAME}" -d "password=${KEYCLOAK_PASSWORD}" -d "grant_type=password" | jq -r '.access_token'`
+echo "Waiting for keycloak..."
+until curl -k https://keycloak:8443 > /dev/null 2>&1; do echo "."; sleep 2; done
+echo "\nDone"
 
-curl -k -XPOST -H "Authorization: bearer ${bearer_token}" -H "Content-Type: application/json" "https://keycloak:8443/auth/admin/realms/Demo/users" \
+echo "Getting bearer token..."
+bearer_token=`curl -ks "https://keycloak:8443/auth/realms/master/protocol/openid-connect/token" -d "client_id=admin-cli" -d "username=${KEYCLOAK_USERNAME}" -d "password=${KEYCLOAK_PASSWORD}" -d "grant_type=password" | jq -r '.access_token'`
+echo "\nDone"
+
+echo "Uploading Demo realm..."
+curl -k -XPOST -H "Authorization: bearer ${bearer_token}" -H "Content-Type: application/json" "https://keycloak:8443/auth/admin/realms" \
+ -d @realm-export.json
+echo "\nDone"
+
+echo "Creating demo user..."
+curl -ks -XPOST -H "Authorization: bearer ${bearer_token}" -H "Content-Type: application/json" "https://keycloak:8443/auth/admin/realms/Demo/users" \
   -d '{
         "username": "demo",
         "firstName": "Demo",
@@ -19,6 +31,4 @@ curl -k -XPOST -H "Authorization: bearer ${bearer_token}" -H "Content-Type: appl
     	    "value": "ch4ng3m3!"
         }]
       }'
-
-curl -k -XPOST -H "Authorization: bearer ${bearer_token}" -H "Content-Type: application/json" "https://keycloak:8443/auth/admin/realms" \
- -d @realm-export.json
+echo "\nDone"
