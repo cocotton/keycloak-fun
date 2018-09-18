4989a73e9da96196d764b21f61a932073e55b3a6
cocotton <julien.parent.trudeau@gmail.com>
2018-09-17 20:57:10 -0400

    Cleanup

--- keycloak-setup/keycloak_setup.sh
+++ keycloak-setup/keycloak_setup.sh
@@ -2,16 +2,13 @@
 
 echo "Waiting for keycloak..."
 until curl -k https://keycloak:8443/auth/ > /dev/null 2>&1; do echo "."; sleep 2; done
-echo "\nDone"
 
 echo "Getting bearer token..."
 bearer_token=`curl -ks "https://keycloak:8443/auth/realms/master/protocol/openid-connect/token" -d "client_id=admin-cli" -d "username=${KEYCLOAK_USERNAME}" -d "password=${KEYCLOAK_PASSWORD}" -d "grant_type=password" | jq -r '.access_token'`
-echo "\nDone"
 
 echo "Uploading Demo realm..."
 curl -k -XPOST -H "Authorization: bearer ${bearer_token}" -H "Content-Type: application/json" "https://keycloak:8443/auth/admin/realms" \
  -d @realm-export.json
-echo "\nDone"
 
 echo "Creating demo user..."
 curl -ks -XPOST -H "Authorization: bearer ${bearer_token}" -H "Content-Type: application/json" "https://keycloak:8443/auth/admin/realms/Demo/users" \
@@ -31,4 +28,6 @@ curl -ks -XPOST -H "Authorization: bearer ${bearer_token}" -H "Content-Type: app
     	    "value": "ch4ng3m3!"
         }]
       }'
-echo "\nDone"
+
+echo ""
+echo "Script completed. Exiting."
