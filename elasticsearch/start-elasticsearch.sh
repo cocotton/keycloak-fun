63c17e05e950b5194a9a9b418f3821c59514136e
cocotton <julien.parent.trudeau@gmail.com>
2018-09-19 20:33:13 -0400

    Fix curl loop

--- elasticsearch/start-elasticsearch.sh
+++ elasticsearch/start-elasticsearch.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 echo "Waiting for keycloak..."
-until curl -k https://keycloak:8443/auth/ > /dev/null 2>&1; do echo "."; sleep 2; done
-
-curl -k -XGET https://keycloak:8443/auth/realms/Demo/protocol/saml/descriptor -o /usr/share/elasticsearch/config/idp-descriptor.xml
+until curl -kf https://keycloak:8443/auth/realms/Demo/protocol/saml/descriptor -o /usr/share/elasticsearch/config/idp-descriptor.xml > /dev/null 2>&1; do echo "."; sleep 2; done
 
 /usr/local/bin/docker-entrypoint.sh
