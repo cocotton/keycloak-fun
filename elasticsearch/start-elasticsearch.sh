7162cf8bb7b8cd3defea071628dfb8046da5ed6a
cocotton <julien.parent.trudeau@gmail.com>
2018-09-19 20:16:36 -0400

    Wait for keycloak to come online before launching keycloak

--- elasticsearch/start-elasticsearch.sh
+++ elasticsearch/start-elasticsearch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-curl -ks -XGET https://keycloak:8443/auth/realms/Demo/protocol/saml/descriptor -o /usr/share/elasticsearch/config/idp-descriptor.xml
+echo "Waiting for keycloak..."
+until curl -k https://keycloak:8443/auth/ > /dev/null 2>&1; do echo "."; sleep 2; done
+
+curl -k -XGET https://keycloak:8443/auth/realms/Demo/protocol/saml/descriptor -o /usr/share/elasticsearch/config/idp-descriptor.xml
 
 /usr/local/bin/docker-entrypoint.sh
